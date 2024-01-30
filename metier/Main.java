198f5b9be5de6df8de6ce7c54ddc095f52edb6e5
Judi <judiherinirina04@gmail.com>
2024-01-30 21:16:29 +0300

    slip word and add to a Array<List>

--- metier/Main.java
+++ metier/Main.java
@@ -1,7 +1,26 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  * Main
  */
 public class Main {
 
-    
+    public static void main(String[] args) throws Exception{
+        String mot = "telephone le plus chere et moin de 1500 Ar";
+        Connection connection = null;
+		try {
+            connection = ConnectionPostgres.connectDefault();
+			connection.setAutoCommit(false);
+			String query = WordFunction.getQuery(mot, connection);
+			System.out.println(query);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+            connection.commit();
+            connection.close();
+        }
+    }
 }
--- metier/WordFunction.java
+++ metier/WordFunction.java
@@ -1,3 +1,20 @@
+
+import java.sql.Connection;
+import java.util.Vector;
+
 public class WordFunction {
+    public static String getQuery(String phrase,Connection connection) throws Exception{
+        // separer le phrase par le espace et le mettre dans un vector
+        String[] phraseSpliter = phrase.split(" ");
+        Vector<String> motList = new Vector<>();
+        for (String string : phraseSpliter) {
+            motList.add(string);
+        }
+
+        // verifier le nom du produit concerner
+        // ... en attente
+        System.out.println(motList);
     
+        return "query";
+    }
 }
